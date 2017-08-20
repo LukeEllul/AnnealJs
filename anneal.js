fdf72d57fd0740f120faf33d1b371f5b54926273
LukeEllul <ellul.luke@gmail.com>
2017-08-20 22:52:10 +0200

    constructed annealing algorithm...now testing

---
+++ anneal.js
@@ -0,0 +1,23 @@
+const R = require('ramda');
+
+const Pacc = T => (cOld, cNew) => Math.exp((cOld - cNew) / T);
+
+const anneal = R.curry(function(solution, cost, neighbor, T, alpha, iter){
+    let oldCost = cost(solution);
+    const tMin = 0.00001;
+    let bestSolution = solution;
+    let t = T;
+    while (t > tMin) {
+        for(let i = 0; i < iter; i++){
+            const newSolution = neighbor(solution);
+            const newCost = cost(newSolution);
+            const ap = Pacc(T)(oldCost, newCost);
+            if(ap > Math.random()){
+                bestSolution = newSolution;
+                oldCost = newCost;
+            }
+        }
+        t = t*alpha;
+    }
+    return {solution, oldCost};
+});
